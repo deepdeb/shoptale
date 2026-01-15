499e92306fbf84ca6cdfa183b6f6e2dc1794941a
deepdeb <debnath.pradeep619@gmail.com>
2026-01-16 00:38:57 +0530

    style changes in HeaderMobileMenuIcon jsx

--- src/components/HeaderElements/HeaderIcons/HeaderMobileMenuIcon.jsx
+++ src/components/HeaderElements/HeaderIcons/HeaderMobileMenuIcon.jsx
@@ -1,10 +1,9 @@
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FaBars } from "react-icons/fa";
 
 export default function HeaderMobileMenuIcon() {
     return (
-        <button className="md:hidden p-2 rounded-md text-gray-600 hover:text-blue-600 focus:outline-none">
-            <FontAwesomeIcon icon={faBars} className="text-xl" />
+        <button className="md:hidden bg-gray-100 hover:bg-blue-100 p-2 rounded-md text-gray-600 hover:text-blue-600 focus:outline-none cursor-pointer">
+            <FaBars className="text-xl" />
         </button>
     )
 }
