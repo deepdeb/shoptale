19c9c3f7b3d595dff12a9cf0725cbc5c5f6eb539
deepdeb <debnath.pradeep619@gmail.com>
2026-01-16 01:11:23 +0530

    HeaderNavigation style changes

--- src/components/HeaderElements/HeaderNavigation.jsx
+++ src/components/HeaderElements/HeaderNavigation.jsx
@@ -1,40 +1,44 @@
 import { faHome, faStore, faChevronDown, faInfoCircle, faEnvelope, faBoxes } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FaBoxes, FaChevronDown, FaEnvelope, FaHome, FaInfoCircle, FaStore } from 'react-icons/fa';
 
 export default function HeaderNavigation() {
+    
+    const baseStyle = 'text-gray-800 hover:text-blue-600 font-medium px-3 py-2 rounded-md hover:bg-blue-50 transition-colors flex items-center';
+
     return (
         <nav className="hidden md:block border-t border-gray-100 py-2">
             <div className="container mx-auto px-4">
                 <ul className="flex items-center justify-center space-x-8">
                     <li>
-                        <a href="#" className="text-gray-800 hover:text-blue-600 font-medium px-3 py-2 rounded-md hover:bg-blue-50 transition-colors flex items-center">
-                            <FontAwesomeIcon icon={faHome} className="mr-2" />
+                        <a href="#" className={baseStyle}>
+                            <FaHome className="mr-2" />
                             Home
                         </a>
                     </li>
                     <li>
-                        <a href="#" className="text-gray-800 hover:text-blue-600 font-medium px-3 py-2 rounded-md hover:bg-blue-50 transition-colors flex items-center">
-                            <FontAwesomeIcon icon={faStore} className="mr-2" />
+                        <a href="#" className={baseStyle}>
+                            <FaStore className="mr-2" />
                             Shop
                         </a>
                     </li>
                     <li className="relative group">
-                        <a href="#" className="text-gray-800 hover:text-blue-600 font-medium px-3 py-2 rounded-md hover:bg-blue-50 transition-colors flex items-center">
-                            <FontAwesomeIcon icon={faBoxes} className="mr-2" />
+                        <a href="#" className={baseStyle}>
+                            <FaBoxes className="mr-2" />
                             Categories
-                            <FontAwesomeIcon icon={faChevronDown} className="ml-1 text-xs" />
+                            <FaChevronDown className="ml-1 text-xs" />
                         </a>
                         {/* Mega menu would go here */}
                     </li>
                     <li>
-                        <a href="#" className="text-gray-800 hover:text-blue-600 font-medium px-3 py-2 rounded-md hover:bg-blue-50 transition-colors flex items-center">
-                            <FontAwesomeIcon icon={faInfoCircle} className="mr-2" />
+                        <a href="#" className={baseStyle}>
+                            <FaInfoCircle className="mr-2" />
                             About
                         </a>
                     </li>
                     <li>
-                        <a href="#" className="text-gray-800 hover:text-blue-600 font-medium px-3 py-2 rounded-md hover:bg-blue-50 transition-colors flex items-center">
-                            <FontAwesomeIcon icon={faEnvelope} className="mr-2" />
+                        <a href="#" className={baseStyle}>
+                            <FaEnvelope className="mr-2" />
                             Contact
                         </a>
                     </li>
