d9c05d3f78ceecba4d588f16345d312a65e3958e
deepdeb <debnath.pradeep619@gmail.com>
2026-01-15 23:38:53 +0530

    updated packages and done some style changes to some components

--- src/components/HeaderElements/HeaderCompanyLogo.jsx
+++ src/components/HeaderElements/HeaderCompanyLogo.jsx
@@ -1,7 +1,7 @@
 export default function HeaderCompanyLogo() {
     return (
-        <div className="flex items-center">
-            <div className="text-3xl font-bold bg-gradient-to-r from-blue-600 to-indigo-700 bg-clip-text text-transparent">
+        <div>
+            <div className="text-3xl font-bold bg-linear-to-r from-blue-600 to-indigo-700 bg-clip-text text-transparent">
                 ShopTale
             </div>
         </div>
--- src/components/HeaderElements/HeaderTopbar.jsx
+++ src/components/HeaderElements/HeaderTopbar.jsx
@@ -1,15 +1,14 @@
-import { faPhone } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FaPhoneAlt } from "react-icons/fa";
 
 export default function HeaderTopbar() {
     return (
-        <div className="bg-gradient-to-r from-blue-600 to-indigo-700 text-white text-sm py-3 px-4">
+        <div className="bg-linear-to-r from-blue-600 to-indigo-700 text-white text-sm py-3 px-4">
             <div className="container mx-auto flex justify-end">
                 <div className="flex items-center space-x-4">
                     <span>Free shipping on orders over $50</span>
                     <span>|</span>
                     <div className="flex items-center space-x-2">
-                        <FontAwesomeIcon icon={faPhone} className="text-xs" />
+                        <FaPhoneAlt className="text-xs" />
                         <span>Support: (123) 456-7890</span>
                     </div>
                 </div>
