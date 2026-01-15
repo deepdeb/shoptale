5d9c5be5fafbeff40f52e8f8bbbe96cdb09e7966
deepdeb <debnath.pradeep619@gmail.com>
2026-01-15 23:51:20 +0530

    style changes in HeaderUserIcon component

--- src/components/HeaderElements/HeaderCompanyLogo.jsx
+++ src/components/HeaderElements/HeaderCompanyLogo.jsx
@@ -2,7 +2,7 @@ export default function HeaderCompanyLogo() {
     return (
         <div>
             <div className="text-3xl font-bold bg-linear-to-r from-blue-600 to-indigo-700 bg-clip-text text-transparent">
-                ShopTale
+                ShopTale.
             </div>
         </div>
     )
--- src/components/HeaderElements/HeaderIcons/HeaderUserIcon.jsx
+++ src/components/HeaderElements/HeaderIcons/HeaderUserIcon.jsx
@@ -1,14 +1,10 @@
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FaUser } from "react-icons/fa";
 
 export default function HeaderUserIcon() {
     return (
         <div className="hidden lg:flex items-center space-x-2 group cursor-pointer">
             <div className="w-10 h-10 flex items-center justify-center rounded-full bg-gray-100 group-hover:bg-blue-100 transition-colors">
-                <FontAwesomeIcon
-                    icon={faUser}
-                    className="text-gray-600 group-hover:text-blue-600 text-lg transition-colors"
-                />
+                <FaUser className="text-gray-600 group-hover:text-blue-600 text-lg transition-colors"/>
             </div>
             <div className="flex flex-col">
                 <span className="text-xs text-gray-500">Hello, Sign in</span>
